5ab1ec69515a3f5868d908053829f00af022975e
rocoide <roodrigo4@hotmail.com>
2023-11-12 18:59:59 -0300

    avanzando con el fetch principal

---
+++ components/carta.js
@@ -0,0 +1,13 @@
+export default function carta(){
+    return `
+        <div class="contenedor-carta">
+            <div class="poster-carta">   
+                <img class="header-img_fondo" src="https://th.bing.com/th/id/OIP.d3cygQpTvFn9vyzQeXYoQAAAAA?pid=ImgDet&rs=1" alt="imagen-sala-cine">
+
+            </div>
+            <div class="descripcion-carta">
+                <h2>Titulo</h2>
+            </div>
+        </div>
+    `
+}
--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+import carta from "./components/carta.js"
+
 window.onload = async function ()  {
     await funciones("","","");
 };
@@ -17,9 +19,31 @@ const funciones = async (titulo, fecha, generoId) =>
         (
             `https://localhost:7220/api/v1/Funcion?titulo=${titulo}&fecha=${fecha}&genero=${generoId}`, config
         );
-        const result = await response.json();
-        console.log(result);
-        return result;
+        if (response.ok === true)
+        {
+            if (response.status === 200)
+            {
+                const result = await response.json();
+                console.log(result);
+                let contenedor = document.getElementById("contenedor-cartas");
+                if (result.length !== 0)
+                {
+                    contenedor.innerHTML = carta();
+                }
+                else
+                {
+                    contenedor.innerHTML = '<h2>No hay funciones actualmente</h2>';
+                }
+            }
+            else
+            {
+                console.log(`El servidor contesto con un ${response.status}`);
+            }
+        }
+        else
+        {
+            console.log("Error de conexion");
+        }  
     } 
     catch (error) 
     {
