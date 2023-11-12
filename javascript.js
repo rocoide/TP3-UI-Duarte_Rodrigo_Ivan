f165d7c6d1f040a23c9cdfdf81cc2dd015f243ab
rocoide <roodrigo4@hotmail.com>
2023-11-12 06:24:27 -0300

    se resolvio el problema de la uri del fetch

--- javascript.js
+++ javascript.js
@@ -1,21 +1,21 @@
-window.onload = function ()  {
-    funciones('','','');
+window.onload = async function ()  {
+    await funciones("","","");
 };
 
 const funciones = async (titulo, fecha, generoId) => 
     {
     const config = {
-        method: "GET",
+        method: 'GET',
         headers: 
         {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json'
         },
     };
     try 
     {
         const response = await fetch
         (
-            `http://localhost:7220/api/v1/Funcion?titulo=${titulo}&fecha=${fecha}&genero=${generoId}`, config
+            `https://localhost:7220/api/v1/Funcion?titulo=${titulo}&fecha=${fecha}&genero=${generoId}`, config
         );
         const result = await response.json();
         console.log(result);
