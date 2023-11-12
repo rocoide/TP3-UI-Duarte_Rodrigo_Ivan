253bef4aa48b607103c21bf087cf5be89b5b7181
rocoide <roodrigo4@hotmail.com>
2023-11-12 04:59:56 -0300

    problemas con el fetch

--- javascript.js
+++ javascript.js
@@ -1,65 +1,28 @@
 window.onload = function ()  {
-    console.log("Ejecutando funciones iniciales");
-    GetFunciones("","","");
+    funciones('','','');
 };
-  
-async function GetFunciones(titulo, fecha, genero) {
-    console.log("Titulo: ", titulo);
-    console.log("Fecha: ", fecha);
-    console.log("Genero: ", genero);
+
+const funciones = async (titulo, fecha, generoId) => 
+    {
     const config = {
-        method: 'GET',
-        headers:{
-            'Content-Type':'application/json'
-        }
+        method: "GET",
+        headers: 
+        {
+            "Content-Type": "application/json",
+        },
     };
-    try{
-        let url2 = `https://localhost:7220/api/v1/Funcion?=Titulo$(titulo)`
-        let UrlBase = 'https://localhost:7220/api/v1/Funcion'
-        if (titulo !== "" || fecha !== "" || genero !== ""){
-            UrlBase = UrlBase + "?"
-            if (titulo !== "")
-            {
-                UrlBase = UrlBase + "Titulo=" + titulo; 
-            };
-            if (fecha !== ""  && titulo !== "")
-            {
-                UrlBase = UrlBase + "&" + "Fecha=" + fecha;
-            }
-            else
-            {
-                if (fecha !== "")
-                {
-                    UrlBase = UrlBase + "Fecha=$" + fecha;
-                }
-            }
-            if (genero != "" && (fecha !== ""  && titulo !== ""))
-            {
-                UrlBase = UrlBase + "&" + "GeneroId=" + genero;
-            }
-            else
-            {
-                if (genero != "")
-                {
-                    UrlBase = UrlBase + "GeneroId=" + genero;
-                }
-            }
-        }
-        const response = await fetch (UrlBase, config);
+    try 
+    {
+        const response = await fetch
+        (
+            `http://localhost:7220/api/v1/Funcion?titulo=${titulo}&fecha=${fecha}&genero=${generoId}`, config
+        );
         const result = await response.json();
         console.log(result);
-    }catch(error){
-        console.log(error);
-        console.log("hola");
+        return result;
+    } 
+    catch (error) 
+    {
+      console.log(error);
     }
-}
-
-document.getElementById("formulario-filtros").addEventListener("submit", function(event) {
-    event.preventDefault(); // Previene el envío del formulario por defecto
-    let titulo = document.getElementById("titulo").value; // Obtén los valores de los campos de entrada
-    let fecha = document.getElementById("fecha").value;
-    let genero = document.getElementById("genero").value;
-    console.log(titulo);
-    GetFunciones(titulo, fecha, genero); // Llama a tu función JavaScript con los datos
-    }
-);
+  };
