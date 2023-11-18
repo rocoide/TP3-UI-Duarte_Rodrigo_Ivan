21cd9f0555e42f3a51db6ea210c743de2e3764cd
rocoide <roodrigo4@hotmail.com>
2023-11-18 07:29:11 -0300

    se continua avanzando con el proyecto, se llego a la parte de hacer el post y generar los tickets

--- javascript/mapeos/botonFuncion.js
+++ javascript/mapeos/botonFuncion.js
@@ -4,7 +4,7 @@ export default function botonFuncion(funcion)
     let fechaEspanish = fecha.toLocaleDateString('es-ES');
     let diaEspanish = fecha.toLocaleDateString('es-ES', { weekday: 'long' });
     return  `
-            <div class="boton-funcion" id="${funcion.funcionId}">
+            <div class="boton-funcion btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" id="${funcion.funcionId}">
                 <p>${diaEspanish} ${fechaEspanish}</p>
                 <p>${funcion.horario}</p>
             </div>
